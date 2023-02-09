730ab61ea5a6049e971383a66a8ee0bee25ba60f
maggie <mjtice0@gmail.com>
2023-02-09 14:24:41 -0700

    added accessibility features for form

---
+++ src/features/Features.js
@@ -0,0 +1,45 @@
+export const features = [
+  {
+    name: "Music",
+  },
+  {
+    name: "Dedicated Quiet Space",
+  },
+  {
+    name: "Outdoor Area",
+  },
+  {
+    name: "Headphones",
+  },
+  {
+    name: "Dedicated Quiet Hours",
+  },
+  {
+    name: "Low Lighting",
+  },
+  {
+    name: "Fluorescent Lighting",
+    
+  }
+  {
+    name: "Natural Ligthing",
+  },
+  {
+    name: "Strobe Lights/Flashing Lights",
+  },
+  {
+    name: "Dedicated Scent-free",
+  },
+  {
+    name: "Scented Air Freshners/Candles",
+  },
+  {
+    name: "Bright Colors",
+  },
+  {
+    name: "Loud Environmental Noise (traffic, busy street)",
+  },
+  {
+    name: "Pets Allowed",
+  },
+];
