6582ab0521a50c17f4fc9532eb24fde113f32919
maggie <mjtice0@gmail.com>
2023-02-09 21:29:48 -0700

    added state to all checkboxes in review form

--- src/components/CreateNewReview.js
+++ src/components/CreateNewReview.js
@@ -2,16 +2,46 @@ import React, { useState } from "react";
 import "./Map.css";
 import { features } from "./features";
 
-const CreateNewReview = () => {
-  const [review, setReview] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
+const CreateNewReview = (props) => {
+  const [reviewTitle, setReviewTitle] = useState("");
+  const [reviewDescription, setReviewDescription] = useState("");
+  const [reviewRating, setReviewRating] = useState("");
+  const [checkedState, setCheckedState] = useState(
+    new Array(features.length).fill(false)
+  );
 
-  // const handleOnChange = () => {
-  //   setIsChecked(!isChecked);
-  // };
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+    index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  }
+  
+  const handleReviewTitle = (event) => {
+    setReviewTitle(event.target.value);
+  }
+  const handleReviewDescription = (event) => {
+    setReviewDescription(event.target.value);
+  }
+
+  const handleReviewRating = (event) => {
+    setReviewRating(event.target.value);
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("form submitted!");
+  
+    const newReview = {
+      title: reviewTitle,
+      description: reviewDescription,
+      rating: reviewRating,
+    };
+
+    props.onSubmitReview(newReview);
+    setReviewTitle("");
+    setReviewDescription("");
+    setReviewRating(0);
   };
 
   return (
@@ -19,11 +49,11 @@ const CreateNewReview = () => {
       {/* <h2 className="review-header">Reviews</h2> */}
       <form className="review-form" onSubmit={handleSubmit}>
       <label>Title:</label>
-      <input type="text" placeholder="Enter a title" name="title" />
+      <input type="text" placeholder="Enter a title" value={reviewTitle} onChange={handleReviewTitle} />
       <label>Description:</label>
-      <input type="text" placeholder="Enter Review" name="description" />
+      <input type="text" placeholder="Enter Review" value={reviewDescription} onChange={handleReviewDescription}/>
       <label>Rating:</label>
-      <select>
+      <select value={reviewRating} onChange={handleReviewRating}>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -42,6 +72,8 @@ const CreateNewReview = () => {
                   id={`custom-checkbox-${index}`}
                   name={name}
                   value={name}
+                  checked={checkedState[index]}
+                  onChange={() => handleOnChange(index)}
                 />
                 <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
               </div>
