9e656a9292efa7118e4490617aa3e859f28a40fa
maggie <mjtice0@gmail.com>
2023-02-13 23:02:17 -0700

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,6 @@ export default function Footer() {
   return (
     <div
       className='footer'
-      style={{
-        color: "white",
-        position:"fixed",
-        bottom: "0",
-        left: "45%",
-      }}
     >
      🎧 © All rights reserved.
     </div>
