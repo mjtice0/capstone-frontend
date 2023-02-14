88d592de5e543c593a8f3b0603f1894b70e64d2f
maggie <mjtice0@gmail.com>
2023-02-13 23:08:43 -0700

    changed footer and css for home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import Map from "./components/Map";
-import AddReviewForm from "./components/AddReviewForm";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Navbar from "./Navbar";
 import Footer from "./components/Footer";
 import PlaceDetails from "./components/PlaceDetails";
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import "./Navbar.css";
-import logo from "./images/accessibilityLogo.png";
 
 function App() {
   const [selectedPlace, setSelectedPlace] = useState(null);
@@ -30,13 +24,14 @@ function App() {
           <Map onMarkerClick={onMarkerClick} />
           <PlaceDetails place={selectedPlace} />
         </div>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
+        <Routes> 
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> */}
+         </Routes>
+
       <div className="footer-container">
         <Footer />
+        </div>
       </div>
     </>
   );
@@ -44,27 +39,3 @@ function App() {
 
 export default App;
 
-// return (
-//     <>
-//       <nav className="navbar">
-//         <Link to="/" className="site-heading">
-//           <img src={logo} alt="accessibility-logo" className="logo" />
-//         </Link>
-//         <ul>
-//           <Link to="/login">Home</Link>
-//           <Link to="/login">Login</Link>
-//           <Link to="/register">Register</Link>
-//         </ul>
-//       </nav>
-
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//       </Routes>
-//       <div className="first-row">
-//          <Map onMarkerClick={onMarkerClick} />
-//          <PlaceDetails place={selectedPlace} />
-//        </div>
-//     </>
-//   );
-// }
